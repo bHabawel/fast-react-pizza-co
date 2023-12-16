39eba79cfb40fdd7308c946ccaf1874d6078c7a6
bHabawel <rosshabawel12@gmail.com>
2023-12-16 13:48:34 +0800

    updating cart quantities

--- src/ui/Button.jsx
+++ src/ui/Button.jsx
@@ -13,6 +13,7 @@ function Button({ children, disabled, to, type, onClick }) {
     focus:text-stone-800 font-semibold uppercase tracking-wide text-stone-400 transition-colors duration-300 
     hover:bg-stone-300 focus:bg-stone-300 focus:outline-none focus:ring focus:ring-stone-200 focus:ring-offset-2 
     active:bg-slate-400 disabled:cursor-not-allowed`,
+    round: base + "px-2.5 py-1 md:px-3.5 md:py-2 text-sm",
   };
   if (to)
     return (
